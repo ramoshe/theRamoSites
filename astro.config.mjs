fc009257951036620b8e39f706d0ae9a240cd9b1
Rachel Sherard <rsherard@mac.com>
2025-04-13 17:24:22 -0400

    update config and stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 import { defineConfig } from 'astro/config';
 import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+	output: 'server',
+	adapter: netlify(),
 	site: "https://ramosites.com",
+	redirects: {
+		'/booking': 'https://book.squareup.com/appointments/vjrjd6n9mj1tfy/location/LK93RM75F2SNS/services',
+		'/consult': 'https://book.squareup.com/appointments/b0okwnpqhl5j7v/location/LK93RM75F2SNS/services',
+		'/website-start': 'https://book.squareup.com/appointments/t1wixfgf35ntau/location/LK93RM75F2SNS/services',
+		'/website-meeting': 'https://book.squareup.com/appointments/l0nwrwucp28fj7/location/LK93RM75F2SNS/services',
+		'/ETSsistas': 'https://book.squareup.com/appointments/kd5ppmciqp3xdh/location/LK93RM75F2SNS/services'
+	},
 	integrations: [
 		robotsTxt(),
 		sitemap(),
