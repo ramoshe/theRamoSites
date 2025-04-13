71880c82633821f677a5dae7b968f8d913f23e97
Rachel Sherard <rsherard@mac.com>
2025-04-13 18:07:39 -0400

    update redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,8 @@ export default defineConfig({
 		'/consult': 'https://book.squareup.com/appointments/b0okwnpqhl5j7v/location/LK93RM75F2SNS/services',
 		'/website-start': 'https://book.squareup.com/appointments/t1wixfgf35ntau/location/LK93RM75F2SNS/services',
 		'/website-meeting': 'https://book.squareup.com/appointments/l0nwrwucp28fj7/location/LK93RM75F2SNS/services',
-		'/ETSsistas': 'https://book.squareup.com/appointments/kd5ppmciqp3xdh/location/LK93RM75F2SNS/services'
+		'/ETSsista': 'https://book.squareup.com/appointments/kd5ppmciqp3xdh/location/LK93RM75F2SNS/services',
+		'/etssista': 'https://book.squareup.com/appointments/kd5ppmciqp3xdh/location/LK93RM75F2SNS/services'
 	},
 	integrations: [
 		robotsTxt(),
